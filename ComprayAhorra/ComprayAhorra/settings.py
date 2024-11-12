970a1480682efec8fbbb453ae29fb1aecc98aa9d
Ruben14f <mansilla8090@gmail.com>
2024-11-12 14:52:49 -0300

    s

--- ComprayAhorra/ComprayAhorra/settings.py
+++ ComprayAhorra/ComprayAhorra/settings.py
@@ -46,9 +46,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware.'
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Corregido aquí
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
