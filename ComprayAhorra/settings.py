8269f5eb852fd782a22e29d45c9dbeae7d31b091
Ruben14f <mansilla8090@gmail.com>
2024-12-11 15:06:22 -0300

    d

--- ComprayAhorra/settings.py
+++ ComprayAhorra/settings.py
@@ -136,11 +136,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
 
